190957db1fde66495c92c07db92125a8eb163290
sacharavya <48620902+sacharavya@users.noreply.github.com>
2020-10-10 15:05:58 +0545

    middleware, backend routes, user dummy data created for functionality of app to test

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+
+import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+
+dotenv.config()
+
+connectDB()
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World...')
+})
+
+app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
+
+//Port is used to get the port for desirable port when deploying the app in the web
+const port = process.env.PORT || 5000
+app.listen(port, () =>
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${port}...`
+  )
+)
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import axios from 'axios'
+
+const ProductScreen = ({ match }) => {
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`)
+
+      setProduct(data)
+    }
+    fetchProduct()
+  }, [match])
+  return (
+    <>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>{product.name}</h2>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} Reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Price:Rs.{product.price}</ListGroup.Item>
+            <ListGroup.Item>Description:{product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>Rs.{product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    <strong>
+                      {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                    </strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  className="btn-block"
+                  type="button"
+                  disabled={product.countInStock === 0}
+                >
+                  Add to Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  )
+}
+
+export default ProductScreen

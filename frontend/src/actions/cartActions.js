ae8f1409bd14ff2187736e8700362d2f26ea2249
sacharavya <48620902+sacharavya@users.noreply.github.com>
2020-10-22 06:30:12 +0545

    css changed and cartaction and cart screen changed

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { CART_ADD_ITEM } from '../constants/cartConstants'
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
-  const { data } = await axios.get(`api/products/${id}`)
+  const { data } = await axios.get(`/api/products/${id}`)
   dispatch({
     type: CART_ADD_ITEM,
     payload: {

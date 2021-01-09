262118fe4ec5a4d57ad7d0fef4533170b654c431
Saurav Acharya <sacharavya@gmail.com>
2021-01-09 10:11:42 +0545

    URL blocking added to admin screen, unauthorized user cannot access admin URL too, and if not logged in will be redirected to login page or if loggedin will be redirected to home page

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -6,15 +6,26 @@ import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { listUsers } from '../actions/userActions'
 
-const UserListScreen = () => {
+const UserListScreen = ({ history }) => {
   const dispatch = useDispatch()
 
   const userList = useSelector((state) => state.userList)
   const { loading, error, users } = userList
 
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
   useEffect(() => {
-    dispatch(listUsers())
-  }, [dispatch])
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers())
+    } else {
+      if (!userInfo) {
+        history.push('/login')
+      } else {
+        history.push('/')
+      }
+    }
+  }, [dispatch, history])
 
   const deleteHandler = (id) => {
     console.log('delete')

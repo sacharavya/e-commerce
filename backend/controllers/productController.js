27ca337ef63a442a161429aa57745f3fbfa29f0b
Saurav Acharya <sacharavya@gmail.com>
2021-01-25 09:20:18 +0545

    commit

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -87,10 +87,48 @@ const updateProduct = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc create new review
+// @route POST /api/products/:id/reviews
+// @access Private
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    const alreadyReviewed = products.reviews.find(
+      (r) => r.user.toString() === req.user._id.toString()
+    )
+    if (alreadyReviewed) {
+      res.status(400)
+      throw new Error('Product already reviewed')
+    }
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    }
+
+    products.reviews.push(review)
+
+    product.numReviews = product.reviews.length
+
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      products.reviews.length
+
+    await product.save()
+  } else {
+    res.status(404)
+    throw new Error('Product not Found')
+  }
+})
+
 export {
   getProducts,
   getProductById,
   deleteProduct,
   createProduct,
   updateProduct,
+  createProductReview,
 }
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
+import morgan from 'morgan'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
@@ -14,6 +15,10 @@ dotenv.config()
 connectDB()
 
 const app = express()
+
+if (process.env.NODE_ENV == 'development') {
+  app.use(morgan('dev'))
+}
 
 app.use(express.json())
 

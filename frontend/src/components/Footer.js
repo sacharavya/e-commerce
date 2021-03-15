919c867fb4c429ad737bf139e77b61f252f002cb
Saurav Acharya <sacharavya@gmail.com>
2021-03-15 19:34:55 +0545

    updated

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,43 +7,11 @@ const Footer = () => {
   }
   return (
     <footer>
-        <Row>
-          <Col className='text-center py-3'>
-            <Row>
-                <Col>This is official website of SARK Enterprises, which is involed in sales of CCTV and networking devices since last few years.</Col>
-            </Row>
-          </Col>
-          <Col className='text-center py-3'>
-            <Row>
-              <ul>
-                <li>Home</li>
-                <li>Categories</li>
-                <li>Contact Us</li>
-              </ul>
-            </Row>
-          </Col>
-          <Col className='text-center py-3'>
-            <Row>
-              Follow Us on:
-                <ul>
-                  <li>
-                    <i className='fab fa-facebook-f'></i>
-                  </li>
-                  <li>
-                  <i className='fab fa-instagram'></i>
-                  </li>
-                  <li>
-                  <i className='fab fa-twitter'></i>
-                  </li>
-                </ul>
-            </Row>
-          </Col>
-        </Row>
-        <Row>
-          <Col className='text-center py-3'>
-            Copyright &copy; Saurav 2020 - {getYear()}
-          </Col>
-        </Row>
+      <Row>
+        <Col className='text-center py-3'>
+          Copyright &copy; Saurav 2020 - {getYear()}
+        </Col>
+      </Row>
     </footer>
   )
 }
